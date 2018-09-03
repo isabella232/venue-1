19e03d758461b3dc80b7d1c1c9eb8adaa5cff90b
Thomas Ochman <thomas@craftacademy.se>
2018-09-03 16:16:59 +0200

    adds css from the original site adds mui css framework as a substitute for bootstrap extracts navigation bar and WIP carousell to partials adds a temporary root_path pointing to application#index adds logo and images from the original site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+    def index
+        # This is a TMP index page
+        @entries = ['This Band That We Love', 'Another Band', 'Amy Lee', 'Some Other']
+    end
 end
