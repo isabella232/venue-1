4102cbf664c26fe29dc8a4286c3d294e200e8d14
Erik <erik.steinman@hotmail.com>
2018-09-03 10:56:54 +0200

    remove one of the FactoryBot-include from rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-    config.include FactoryBot::Syntax::Methods
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = true
     config.infer_spec_type_from_file_location!
