c46653b6460df408f257a4772c34b9118d0ed883
Erik <erik.steinman@hotmail.com>
2018-09-04 17:02:33 +0200

    edit env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -17,7 +17,7 @@ Cucumber::Rails::Database.javascript_strategy = :truncation
 Chromedriver.set_version '2.33'
  Capybara.register_driver :selenium do |app|
   options = Selenium::WebDriver::Chrome::Options.new(
-      args: %w( headless disable-popup-blocking disable-infobars)
+      args: %w(disable-popup-blocking disable-infobars)
   )
    Capybara::Selenium::Driver.new(
       app,
