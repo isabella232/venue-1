4eabe6934745d5e941f54638e78bb1ab57a8dab0
Erik <erik.steinman@hotmail.com>
2018-09-04 17:02:07 +0200

    add step_definitions

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -2,6 +2,6 @@ Then("I should be redirected to the Create New Campaign page") do
     expect(current_path).to eq new_campaign_path
 end
 
-Then("there should be a Campaign titled {string} in the Database") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Then("there should be a Campaign titled {string} in the Database") do |expected_title|
+    expect(Campaign.last.title).to eq expected_title
 end
