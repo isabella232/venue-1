75949069597616b0e2038e6a6f20722fa6c1dc45
Erik <erik.steinman@hotmail.com>
2018-09-04 17:03:32 +0200

    add create-action for campagins_controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -5,4 +5,20 @@ class CampaignsController < ApplicationController
   def new
     @campaign = Campaign.new
   end
+
+  def create
+    @campaign = Campaign.create(campaign_params)
+    if @campaign.persisted?
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+
+def campaign_params
+  params.require(:campaign).permit(:title, :description)
+end
+
 end
