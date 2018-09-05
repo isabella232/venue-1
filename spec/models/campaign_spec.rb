34b2ef942a6c8c9f4486d3f264724e9801a32988
AndreasMN <andycutter@godmail.dk>
2018-09-05 14:27:37 +0200

    add RSpec testing for campaign and user

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Campaign, type: :model do
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_presence_of :location }
 
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:campaign)).to be_valid 
+    end
+  end
+
 end
