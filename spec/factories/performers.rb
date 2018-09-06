767f5777c83a00e61f4527372e342cbc3c323e80
AndreasMN <andycutter@godmail.dk>
2018-09-06 17:27:15 +0200

    add test for performer factory, and assiciations

--- spec/factories/performers.rb
+++ spec/factories/performers.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :performer do
-    
+    name { "MyString" }
+    genre { "MyString" }
+    city { "MyString" }
+    description { 'Mytext' }
+    facebook { "MyString" }
+    instagram { "MyString" }
+    twitter { "MyString" }
+    website { "MyString" }
+    spotify { "MyString" }
   end
 end
--- spec/models/performer_spec.rb
+++ spec/models/performer_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Performer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'DB table' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :genre }
+    it { is_expected.to validate_presence_of :city }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :facebook }
+    it { is_expected.to validate_presence_of :instagram }
+    it { is_expected.to validate_presence_of :twitter }
+    it { is_expected.to validate_presence_of :website }
+    it { is_expected.to validate_presence_of :spotify }
+  end
+  
+  describe 'Associations' do
+    it { is_expected.to have_and_belong_to_many :user }
+  end
+  
+  describe "Factory" do
+    it "for :user is valid" do
+      expect(create(:performer)).to be_valid
+    end
+  end
 end
