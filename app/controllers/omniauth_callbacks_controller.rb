8be570c31eeb41759618586c326e8d5c12c3785b
AndreasMN <andycutter@godmail.dk>
2018-09-06 11:26:46 +0200

    missing underscore in file name

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    
+    def facebook
+        @user = User.from_omniauth(request.env['omniauth.auth'])
+        if @user.persisted?
+            sign_in_and_redirect @user, event: :authentication
+            set_flash_message(:notice, :succes, kind: 'Facebook') if is_navigational_format?
+        else
+            session['devise.facebook_date'] = request.env['omniauth.auth']
+            redirect_to new_user_registration_path
+        end
+    end
+end
