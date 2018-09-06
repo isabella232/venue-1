cd1631c93e43b8d2b583b10d300460221bc1b10c
Erik <erik.steinman@hotmail.com>
2018-09-06 23:01:14 +0200

    rails db:drop db:create db:migrate to get rid of non-essentail DB-rows for this PR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_123249) do
+ActiveRecord::Schema.define(version: 2018_09_06_093035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,20 +23,6 @@ ActiveRecord::Schema.define(version: 2018_09_06_123249) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_campaigns_on_user_id"
-  end
-
-  create_table "performers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "genre"
-    t.string "city"
-    t.string "description"
-    t.string "facebook"
-    t.string "instagram"
-    t.string "twitter"
-    t.string "website"
-    t.string "spotify"
   end
 
   create_table "users", force: :cascade do |t|
