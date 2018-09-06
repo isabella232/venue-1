56a6ea9f52a305955b7ce6e82eebbaddb565f4bc
AndreasMN <andycutter@godmail.dk>
2018-09-06 11:13:10 +0200

    add omniauth to routes and use actual link in sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: :registrations
+    registrations: :registrations,
+    omniauth_callbacks: :omniauth_callbacks
   }
   root controller: :campaigns, action: :index
   resources :campaigns, only: [:index, :create, :new, :show]
