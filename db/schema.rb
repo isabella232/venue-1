cd353401b374972f848e4adfe665b965e1b2d592
Erik <erik.steinman@hotmail.com>
2018-09-06 14:38:18 +0200

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_112938) do
+ActiveRecord::Schema.define(version: 2018_09_06_123249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,15 +25,9 @@ ActiveRecord::Schema.define(version: 2018_09_06_112938) do
     t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+  create_table "performers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
     t.string "name"
     t.string "genre"
     t.string "city"
@@ -43,6 +37,17 @@ ActiveRecord::Schema.define(version: 2018_09_06_112938) do
     t.string "twitter"
     t.string "website"
     t.string "spotify"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
