49d97aa0ac9c81c24ad7724e63753f894b422c03
Thomas Ochman <thomas@craftacademy.se>
2018-09-07 21:01:24 +0200

    adds a dot :zap:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < Devise::SessionsController
     self.resource = warden.authenticate(auth_options)
     if self.resource
       sign_in(resource_name, resource)
-      flash[:notice] = 'Signed in successfully'
+      flash[:notice] = 'Signed in successfully.'
       render js: "Turbolinks.visit(Turbolinks.absoluteURL, {action: 'reload'})"
     else
       render json: {message: 'Could not log you in'}, status: 422
