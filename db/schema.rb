a296c03a92f69a0e1da22f73a4eb7d02165c6da1
Erik <erik.steinman@hotmail.com>
2018-09-07 16:14:10 +0200

    add foreign_key, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_135058) do
+ActiveRecord::Schema.define(version: 2018_09_07_140324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2018_09_07_135058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "tickets_id"
+    t.index ["tickets_id"], name: "index_campaigns_on_tickets_id"
     t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
 
@@ -50,6 +52,8 @@ ActiveRecord::Schema.define(version: 2018_09_07_135058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.bigint "campaign_id"
+    t.index ["campaign_id"], name: "index_tickets_on_campaign_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,5 +71,7 @@ ActiveRecord::Schema.define(version: 2018_09_07_135058) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "campaigns", "tickets", column: "tickets_id"
   add_foreign_key "campaigns", "users"
+  add_foreign_key "tickets", "campaigns"
 end
