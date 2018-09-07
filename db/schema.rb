520f5c23e02e024b84e297d77055ed9e88073652
Erik <erik.steinman@hotmail.com>
2018-09-07 15:54:28 +0200

    AddPriceToTickets, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_142324) do
-
+ActiveRecord::Schema.define(version: 2018_09_07_135058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +44,12 @@ ActiveRecord::Schema.define(version: 2018_09_06_142324) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_campaigns_on_user_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
