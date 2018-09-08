80d16b72941c1ec83eec8254d02f988f1e7f2ae7
Erik <erik.steinman@hotmail.com>
2018-09-08 11:06:21 +0200

    add accepts_nested_attributes_for :tickets to Campaign-model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,9 @@ class Campaign < ApplicationRecord
     validates_presence_of :title, :description, :location 
     
     belongs_to :user
-    has_many :tickets
+
     has_one_attached :image
+
+    has_many :tickets
+    accepts_nested_attributes_for :tickets
 end
