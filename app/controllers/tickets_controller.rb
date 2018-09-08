0c4cd72d3489903a1fa3b1558235d4c8349aa9c8
Erik <erik.steinman@hotmail.com>
2018-09-08 11:08:51 +0200

    add new-, create- and show-actions to new Tickets-controller

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,27 @@
+class TicketsController < ApplicationController
+    before_action :authenticate_user!, only: [:create]
+
+    def new
+        @ticket = Ticket.new
+        authorize @ticket
+    end
+    
+    def create
+        @ticket = current_user.tickets.create(tickets_params)
+        if @ticket.persisted?
+        redirect_to campaign_path(@campaign)
+        else
+        render json: {message: "Additional input required"}, status: 422
+        end
+    end
+
+    def show
+        @ticket = Ticket.find(params[:id])
+      end
+
+    private 
+
+    def tickets_params
+        params.require(:ticket).permit(:price, :name)
+    end
+end
