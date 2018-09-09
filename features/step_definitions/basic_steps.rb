a382faaf63252dd86a77d764f7d7bfb49887caee
Thomas Ochman <thomas@craftacademy.se>
2018-09-09 11:59:36 +0200

    .zap:

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -6,7 +6,7 @@ end
 And ("I click( on) {string} detail box") do |campaign_title|
     campaign = Campaign.find_by(title: campaign_title)
     within "##{ActionView::RecordIdentifier.dom_id(campaign)}" do 
-        click_on link
+        click_on campaign_title
     end
 end
 
