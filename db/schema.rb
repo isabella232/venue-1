804e21010443531076d575d557f27b60f19e947c
Erik <erik.steinman@hotmail.com>
2018-09-10 10:58:57 +0200

    solve merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_211316) do
+ActiveRecord::Schema.define(version: 2018_09_09_164410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2018_09_07_211316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "state"
+    t.integer "campaign_id"
     t.bigint "tickets_id"
     t.index ["tickets_id"], name: "index_campaigns_on_tickets_id"
     t.index ["user_id"], name: "index_campaigns_on_user_id"
