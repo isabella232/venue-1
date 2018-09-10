723dbe0bcff6f00b206842bc7ee95e24f07439fa
Thomas <thomas@craftacademy.se>
2018-09-10 20:41:37 +0200

    removes unused association - for now...

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,8 +2,6 @@ class Campaign < ApplicationRecord
   validates_presence_of :title, :description, :location, :state
 
   belongs_to :user
-  # make this happend
-  has_one :performer
 
   has_one_attached :image
   has_many :tickets
