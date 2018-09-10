ddaae5f6a19c728579834c0b6fc73601855d76bb
Thomas <thomas@craftacademy.se>
2018-09-10 18:09:09 +0200

    updates the associations between user -> campaign -> performer

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,4 +1,5 @@
 class Performer < ApplicationRecord
     validates_presence_of :name, :genre, :city, :description
-    has_and_belongs_to_many :user
+    has_and_belongs_to_many :users
+    has_many :campaigns, through: :users
 end
