885a2154dfcba2c2338454674f0c351153a5f60b
Erik <erik.steinman@hotmail.com>
2018-09-10 19:49:49 +0200

    add purchases_controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,18 @@
 class PurchaseController < ApplicationController
-  def create
+
+  def create_ticket
+    user = User.find(params[:user_id])
+    customer = Stripe::Customer.create(email: user.email, source: params['stripeToken'])
+    charge = Stripe::Charge.create(customer: customer.id, amount: (10 * 100), description: 'Venue Ticket', currency: 'sek')
+    if charge.paid?
+        ticket.created
+        flash[:notice] = 'Your Ticket has been booked!'
+      else
+        flash[:error] = 'Could not subscribe to D-pressed'
+    end
+    redirect_back(fallback_location: root_path)
   end
+
 
   def new
   end
