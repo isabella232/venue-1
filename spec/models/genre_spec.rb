09e239f3dac10130b1213fbc39e8c0f2e94f9016
Siianko <tsiyanko@gmail.com>
2018-09-11 09:27:39 +0200

    add nested factory bot for performer

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Genre, type: :model do
-  
+RSpec.describe Genre, type: :model do  
   describe 'DB table' do
     it { is_expected.to validate_presence_of :name }
   end
@@ -10,10 +9,13 @@ RSpec.describe Genre, type: :model do
     it { is_expected.to belong_to :performer }
   end
 
-  # describe 'Factory' do
-  #   it 'should have valid Factory' do
-  #     expect(create(:genre)).to be_valid 
-  #   end
-  # end
-
+  describe 'Factory' do
+      before :each do
+      @performer = FactoryBot.create(:performer)
+      
+      it 'should have valid Factory' do
+            expect(create(:genre)).to be_valid
+          end
+    end
+  end
 end
