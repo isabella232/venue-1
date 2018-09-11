ea1b3fe89ae87870e57005a14b7396c46b59e7be
Siianko <tsiyanko@gmail.com>
2018-09-11 05:55:38 +0200

    got NoMethodError: NoMethodError:  for Genre, put has_and_belongs_to_many :genres on dif lines, getting another error

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,6 +1,7 @@
 class Performer < ApplicationRecord
     validates_presence_of :name, :genre, :city, :description
-    has_and_belongs_to_many :users, :genres
+    has_and_belongs_to_many :users
+    has_and_belongs_to_many :genres
     has_many :campaigns, through: :users
 end
  
