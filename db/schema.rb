4ba05f9393248402b794b99569922805768b8873
AndreasMN <andycutter@godmail.dk>
2018-09-11 16:23:45 +0200

    add state to performer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_211316) do
+ActiveRecord::Schema.define(version: 2018_09_11_142118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2018_09_07_211316) do
     t.string "website"
     t.string "spotify"
     t.string "youtube"
+    t.string "state"
   end
 
   create_table "performers_users", id: false, force: :cascade do |t|
