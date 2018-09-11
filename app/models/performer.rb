aee048583d6ed9601dd9bb9df045fd3cd2d84b1c
Siianko <tsiyanko@gmail.com>
2018-09-11 05:37:38 +0200

    add has_and_belongs_to_many genres in Performer model

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,6 +1,6 @@
 class Performer < ApplicationRecord
     validates_presence_of :name, :genre, :city, :description
-    has_and_belongs_to_many :users
+    has_and_belongs_to_many :users, :genres
     has_many :campaigns, through: :users
 end
  
