0812ae80fc87b708c0111a4a054f8bd718556fa4
AndreasMN <andycutter@godmail.dk>
2018-09-11 15:09:47 +0200

    add feature file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
 fan = User.create(email: 'fan@venue.se', password: 'my-password', role: 'fan')
 artist = User.create(email: 'artist@venue.se', password: 'my-password', role: 'artist')
-
-fan = User.create(email: 'fan@venue.se', password: 'my-password', role: 'fan')
-artist = User.create(email: 'artist@venue.se', password: 'my-password', role: 'artist')
 admin = User.create(email: 'admin@venue.se', password: 'my-password', role: 'admin')
+
+artist.performers.create(
+    name: 'Clare Cunningham',
+    genre: 'Rock',
+    city: 'Stockholm',
+    description: 'Dubbed as having a vocal register similar to Adele (GAFFA) and for having a "Powerfull and killer voice" from Lzzy Hale (Halestorm) and Phil Campell (Motörhead) it is no wonder Clare Cunningham is making waves across the world with her music. Storytelling with unwavering honesty is what Cunningham is best at, and this little country rock/pop chick has proven she is a multi versatile recording and live artist, singing across many genres.',
+    facebook: 'https://www.facebook.com',
+    instagram: 'https://www.instagram.com',
+    twitter: 'https://www.twitter.com',
+    youtube: 'https://www.youtube.com',
+    website: 'https://www.google.com',
+    spotify: 'https://www.spotify.com',
+    state: 'active'  
+)
 
 campaigns = Campaign.create([
     {
@@ -40,17 +51,5 @@ campaigns = Campaign.create([
 Campaign.all.each do |campaign|
     campaign.image.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'dummy.jpg')), filename: "image.jpg", content_type: 'image/jpg')
 end
-
-artist.performers.create(
-    name: 'Clare Cunningham',
-    genre: 'Rock',
-    city: 'Stockholm',
-    description: 'Dubbed as having a vocal register similar to Adele (GAFFA) and for having a "Powerfull and killer voice" from Lzzy Hale (Halestorm) and Phil Campell (Motörhead) it is no wonder Clare Cunningham is making waves across the world with her music. Storytelling with unwavering honesty is what Cunningham is best at, and this little country rock/pop chick has proven she is a multi versatile recording and live artist, singing across many genres.',
-    facebook: 'https://www.facebook.com',
-    instagram: 'https://www.instagram.com',
-    twitter: 'https://www.twitter.com',
-    youtube: 'https://www.youtube.com',
-
-    spotify: 'https://www.spotify.com'   
-    )
-
+campaign = Campaign.with_state(:accepted).first
+campaign.tickets.create(name: 'Sure thing', price: 200)
---
+++ features/admin_archive_artist_profile_performer.feature
@@ -0,0 +1,21 @@
+@javascript
+Feature: Admin can archove Artist-profile
+    As an Admin
+    In order to be in control of the content of the Artist profiles
+    I would like to be able to edit and delete Artist profile
+
+    Background:
+        Given that the following Performer exist
+        | name       | state  |
+        | Kanye West | active |
+        And that the following user exist
+        | email          | role  |
+        | admin@venue.se | admin |
+        And I am logged in as 'admin@venue.se'
+
+    Scenario: Admin archives a no longer relevant Performer
+        When I am on the Performer page for 'Kanye West'
+        And I click on 'Archive'
+        Then the state of 'Kanye West' should be 'archived'
+        And I should see 'Performer has been archived'
+        And I should be redirected to the 'Performers' page
