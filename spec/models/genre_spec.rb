af5bc2ede5a1d9b6d11e713d57c675fd604c50d5
Siianko <tsiyanko@gmail.com>
2018-09-11 05:51:56 +0200

    add rspec for Genre

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'DB table' do
+    it { is_expected.to validate_presence_of :name }
+  end
+  
+  describe 'Associations' do
+  it { is_expected.to belong_to :performer }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:genre)).to be_valid 
+    end
+  end
+  
 end
