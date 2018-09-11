71afa4e5bd6af7ee3438ca02c31e75ae8da14b20
Thomas <thomas@craftacademy.se>
2018-09-11 20:24:27 +0200

    WIP payment flow

--- app/assets/javascripts/stripe_payment.js
+++ app/assets/javascripts/stripe_payment.js
@@ -1,3 +1,37 @@
+const stripeTokenHandler = (token) => {
+    // Insert the token ID into the form so it gets submitted to the server
+    var form = document.getElementById('payment-form');
+    var hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
+
+    // Submit the form
+    form.submit();
+}
+
+// Create a token or display an error when the form is submitted.
+const activateForm = (stripe, card) => {
+    var form = document.getElementById('payment-form');
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            stripe.createToken(card).then(function (result) {
+                if (result.error) {
+                    // Inform the customer that there was an error.
+                    showToast('Error', result.error.message, { target: '#card-errors' });
+                } else {
+                    // Send the token to your server.
+                    stripeTokenHandler(result.token);
+                }
+            });
+        });
+    }
+}
+
+
+
 const initiateStripe = (pkKey) => {
     var stripe = Stripe(pkKey);
     var elements = stripe.elements();
@@ -25,40 +59,25 @@ const initiateStripe = (pkKey) => {
     // Add an instance of the card Element into the `card-element` <div>.
     card.mount('#card-element');
     card.addEventListener('change', function (event) {
-        var displayError = document.getElementById('card-errors');
         if (event.error) {
-            displayError.textContent = event.error.message;
+            showToast('Error', event.error.message, { target: '#card-errors' });
         } else {
-            displayError.textContent = '';
+            var toast = document.querySelector('.iziToast'); // Selector of your toast
+            if (toast) {
+                iziToast.hide({}, toast);
+            }
         }
     });
-    // Create a token or display an error when the form is submitted.
-    var form = document.getElementById('payment-form');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
 
-        stripe.createToken(card).then(function (result) {
-            if (result.error) {
-                // Inform the customer that there was an error.
-                var errorElement = document.getElementById('card-errors');
-                errorElement.textContent = result.error.message;
-            } else {
-                // Send the token to your server.
-                stripeTokenHandler(result.token);
-            }
-        });
-    });
+    // Activate the form actions
+    activateForm(stripe, card);
 
-    function stripeTokenHandler(token) {
-        // Insert the token ID into the form so it gets submitted to the server
-        var form = document.getElementById('payment-form');
-        var hiddenInput = document.createElement('input');
-        hiddenInput.setAttribute('type', 'hidden');
-        hiddenInput.setAttribute('name', 'stripeToken');
-        hiddenInput.setAttribute('value', token.id);
-        form.appendChild(hiddenInput);
-
-        // Submit the form
-        form.submit();
-    }
 }
+
+
+
+
+
+
+
+
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,10 +1,26 @@
+# frozen_string_literal: true
+
 class PaymentsController < ApplicationController
   def new
     @order = Order.find(session[:order_id])
   end
 
   def create
-    binding.pry
-    @message = "You rock!"
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: 10000,
+      description: 'Venue tickets',
+      currency: 'sek'
+    )
+      if charge.paid?
+        @message = 'You rock!'
+      else
+        @message = 'You rock!'
+      end
   end
 end
