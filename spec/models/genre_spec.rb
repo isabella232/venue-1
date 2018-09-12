3fd6a7994287f5a73c41b4dc4bd6a6b980cf6f6e
Siianko <39467289+Siianko@users.noreply.github.com>
2018-09-12 12:25:44 +0200

    Update genre_spec.rb

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe Genre, type: :model do
     it { is_expected.to validate_presence_of :name }
   end
 
-  describe 'Factory' do
-      before :each do
-      @performer = FactoryBot.create(:performer)
+ describe 'Factory' do
+   before :each do
+   @performer = FactoryBot.create(:performer)
       
-      it 'should have valid Factory' do
-            expect(create(:genre)).to be_valid
-      end
+  it 'should have valid Factory' do
+    expect(create(:genre)).to be_valid
     end
   end
+end
 
   describe 'Associations' do
     it { is_expected.to have_many(:performers) }
