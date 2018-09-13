712576f554db6c18b90bdd80ef596c37c6c4d4dc
Thomas <thomas@craftacademy.se>
2018-09-13 09:46:21 +0200

    fixes typo in migration name

--- db/migrate/20180912181112_campaigns_genres.rb
+++ db/migrate/20180912181112_campaigns_genres.rb
@@ -1,4 +1,4 @@
-class CampaignsGenras < ActiveRecord::Migration[5.2]
+class CampaignsGenres < ActiveRecord::Migration[5.2]
   create_join_table :campaigns, :genres do |t|
     t.references :campaign
     t.references :genre
