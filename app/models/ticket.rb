7cb15ef43e8ad9cf78f2c7ea9650d180b97888d1
Thomas <thomas@craftacademy.se>
2018-10-10 23:05:13 +0200

    adds #sould_count to ticket and displays count on campaign box adds ticket price display in currency format

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,8 @@ class Ticket < ApplicationRecord
   validates_length_of :ticket_variants, maximum: 3
 
   belongs_to :campaign
+
+  def sold_count
+    self.ticket_variants.map{|variant| variant.sold_count}.inject(:+)
+  end
 end
