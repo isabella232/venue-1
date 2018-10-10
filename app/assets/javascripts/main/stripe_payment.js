2bdfa3fef43c0a378fdaa85039ccfe0f5d79b55a
Thomas <thomas@craftacademy.se>
2018-10-10 18:35:42 +0200

    adds required js code for proper handling of stripe form

--- app/assets/javascripts/main/stripe_payment.js
+++ app/assets/javascripts/main/stripe_payment.js
@@ -1,3 +1,108 @@
+const registerElements = (elements, exampleName) => {
+    var formClass = '.' + exampleName;
+    var example = document.querySelector(formClass);
+  
+    var form = example.querySelector('form');
+    var resetButton = example.querySelector('a.reset');
+    var error = form.querySelector('.error');
+    var errorMessage = error.querySelector('.message');
+  
+    const enableInputs = () => {
+      Array.prototype.forEach.call(
+        form.querySelectorAll(
+          "input[type='text'], input[type='email'], input[type='tel']"
+        ),
+        function(input) {
+          input.removeAttribute('disabled');
+        }
+      );
+    }
+  
+    const disableInputs = () => {
+      Array.prototype.forEach.call(
+        form.querySelectorAll(
+          "input[type='text'], input[type='email'], input[type='tel']"
+        ),
+        function(input) {
+          input.setAttribute('disabled', 'true');
+        }
+      );
+    }
+  
+    // Listen for errors from each Element, and show error messages in the UI.
+    elements.forEach((element) => {
+      element.on('change', (event) => {
+        if (event.error) {
+          error.classList.add('visible');
+          errorMessage.innerText = event.error.message;
+        } else {
+          error.classList.remove('visible');
+        }
+      });
+    });
+  
+    // Listen on the form's 'submit' handler...
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+      // Show a loading screen...
+      example.classList.add('submitting');
+  
+      // Disable all inputs.
+      disableInputs();
+  
+      // Gather additional customer data we may have collected in our form.
+      var name = form.querySelector('#' + exampleName + '-name');
+      var address1 = form.querySelector('#' + exampleName + '-address');
+      var city = form.querySelector('#' + exampleName + '-city');
+      var zip = form.querySelector('#' + exampleName + '-zip');
+      var additionalData = {
+        name: name ? name.value : undefined,
+        address_line1: address1 ? address1.value : undefined,
+        address_city: city ? city.value : undefined,
+        address_zip: zip ? zip.value : undefined,
+      };
+  
+      // Use Stripe.js to create a token. We only need to pass in one Element
+      // from the Element group in order to create a token. We can also pass
+      // in the additional customer data we collected in our form.
+      stripe.createToken(elements[0], additionalData).then((result) => {
+        // Stop loading!
+        example.classList.remove('submitting');
+  
+        if (result.token) {
+          // If we received a token, show the token ID.
+          example.querySelector('.token').innerText = result.token.id;
+          example.classList.add('submitted');
+        } else {
+          // Otherwise, un-disable inputs.
+          enableInputs();
+        }
+      });
+    });
+  
+    resetButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      // Resetting the form (instead of setting the value to `''` for each input)
+      // helps us clear webkit autofill styles.
+      form.reset();
+  
+      // Clear each Element.
+      elements.forEach((element) => {
+        element.clear();
+      });
+  
+      // Reset error state as well.
+      error.classList.remove('visible');
+  
+      // Resetting the form does not un-disable inputs, so we need to do it separately:
+      enableInputs();
+      example.classList.remove('submitted');
+    });
+  }
+
+
+
 const stripeTokenHandler = (token) => {
     var form = document.getElementById('payment-form');
     var hiddenInput = document.createElement('input');
@@ -47,9 +152,6 @@ const addEventListenersToStripeForm = () => {
         });
     });
 }
-
-
-
 
 const initiateStripe = (pkKey) => {
     var stripe = Stripe(pkKey);
